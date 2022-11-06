8ee35ab4fb9f87d599318169b1ada11f439cc1f5
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2022-11-06 16:36:57 +0530

    Update calc.cpp
    Polishing

--- calc.cpp
+++ calc.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int add(int x,int y){
+float add(float x, float y){
 	return x+y;
 }
 
-int substract(int x, int y){
+float substract(float x, float y){
 	return x-y;
 }
 
-int multiply(int x, int y){
+float multiply(float x, float y){
 	return x*y;
 }
 
-float exp(float base, int exp){
+float divide(float x, float y){
+	return x/y;
+}
+
+float exp(float base, float exp){
 	float val = 1;
-	for(int i=0; i<exp; i++){
+	for(float i=0; i<exp; i++){
 		val = val * base;
 	}
 	return val;
@@ -27,19 +30,33 @@ float sin(float theta){
 	return res;
 }
 
-int main() {
-	cout << "Select operation : " << endl;
-	cout << "1. Addition" << endl;
-	cout << "2. Substraction" << endl;
-	cout << "3. Multiplication" << endl;
-	cout << "5. Sin" << endl;
-	int mode;
-	cin >> mode;
-	float res = 0;
+float cos(float theta){
+	float res = 1 - ((exp(theta, 2))/2) + ((exp(theta, 4))/24);
+	return res;
+}
+
+float tan(float theta){
+	float res = theta + ((exp(theta, 3))/3) + ((exp(theta, 5))* (2/225));
+	return res;
+}
+
+void start(){
+		cout << "Select operation : " << endl;
+		cout << "1. Addition" << endl;
+		cout << "2. Substraction" << endl;
+		cout << "3. Multiplication" << endl;
+		cout << "4. Division" << endl;
+		cout << "5. Exponentiation" << endl;
+		cout << "6. Sin" << endl;
+		cout << "7. Cos" << endl; 
+		cout << "8. Tan" << endl;
+		int mode;
+		cin >> mode;
 	
-	
-	switch(mode){
-		int a, b;
+		float res = 0;
+
+		switch(mode){
+		float a, b;
 
 		case 1:
 			cout << "Enter first number" << endl;
@@ -65,9 +82,27 @@ int main() {
 			res = multiply(a,b);
 			break;
 			
+		case 4:
+			cout << "Enter first number" << endl;
+			cin >> a;
+			cout << "Enter second number" << endl;
+			cin >> b;
+			res = divide(a,b);
+			break;
+			
 		case 5:
+			cout << "Enter the base" << endl;
+			float base;
+			cin >> base;
+			cout << "Enter the power" << endl;
+			float power;
+			cin >> power;
+			res = exp(base, power);
+			break;
+			
+		case 6:
 			cout << "For degree input enter 1 and for radians enter 2" << endl;
-			int unit;
+			float unit;
 			cin >> unit;
 			cout << "Enter angle value" << endl;
 			float angle;
@@ -77,13 +112,49 @@ int main() {
 			}
 			res = sin(angle);
 			break;
+			
+		case 7:
+			cout << "For degree input enter 1 and for radians enter 2" << endl;
+			//int unit;
+			cin >> unit;
+			cout << "Enter angle value" << endl;
+			//float angle;
+			cin >> angle;
+			if(unit == 1){
+				angle = angle * 0.01745;
+			}
+			res = cos(angle);
+			break;
+			
+		case 8:
+			cout << "For degree input enter 1 and for radians enter 2" << endl;
+			//int unit;
+			cin >> unit;
+			cout << "Enter angle value" << endl;
+			//float angle;
+			cin >> angle;
+			if(unit == 1){
+				angle = angle * 0.01745;
+			}
+			res = tan(angle);
+			break;
 		
 		default:
 			break;
 	}
 	
-	cout << res << endl;
+	cout << "Result : " << res << endl;
+}
+
+int main() {
+	start();
+		
+	cout << "Enter 1 to restart or 0 to exit" << endl;
+	int reset;
+	cin >> reset;
+	if (reset==1){
+		start();
+	}
 	
 	return 0;
-	
 }
